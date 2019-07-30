4de2471c7fe315ded7fefdced1210190c57b0338
Donghao Wu <donghao.wu.us@gmail.com>
2019-07-30 12:36:21 -0400

    working on functionality

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const mysqlConnection = mysql.createConnection({
   multipleStatements: true
 });
 
-//check connection is running or not
+//check connection is running or not, handle errors
 mysqlConnection.connect(err => {
   if (!err) console.log(`DB connection succeded`);
   else
@@ -27,69 +27,72 @@ mysqlConnection.connect(err => {
     );
 });
 
-app.get(`/employees`,(req,res) =>{
-  mysqlConnection.query('SELECT * FROM Employee', (err, rows, field) =>{
-    if(!err){
+app.get(`/employees`, (req, res) => {
+  mysqlConnection.query("SELECT * FROM Employee", (err, rows, field) => {
+    if (!err) {
       res.send(rows);
       //console.log(rows);
-    }
-    else console.log(err)
-  })
-})
+    } else console.log(err);
+  });
+});
 
-app.get(`/employees/:id`,(req,res) =>{
-  mysqlConnection.query('SELECT * FROM Employee WHERE EmpID =?',
-  [req.params.id],
-  (err, rows, field) =>{
-    if(!err){
-      res.send(rows);
-      console.log(rows);
-    }
-    else console.log(err)
-  })
-})
-//Delete an item by argument
-app.delete(`/employees/:id`,(req,res) =>{
-  mysqlConnection.query('DELETE FROM Employee WHERE EmpID =?',
-  [req.params.id],
-  (err, rows, field) =>{
-    if(!err) res.send('Deleted successfully');
-    else console.log(err)
-  })
-})
-//Add an new item without input EmpId
-app.post('/employees', (req, res) => {
-  let emp = req.body;
-  console.log(emp)
-  emp.EmpID = 0;
-  
-  let sql = 'SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-  CALL EmployeeAddOrEdit(@EmpID, @Name, @EmpCode, @Salary)';
+app.get(`/employees/:id`, (req, res) => {
   mysqlConnection.query(
-    sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    "SELECT * FROM Employee WHERE EmpID =?",
+    [req.params.id],
+    (err, rows, field) => {
+      if (!err) {
+        res.send(rows);
+        console.log(rows);
+      } else console.log(err);
+    }
+  );
+});
+//Delete an item by argument
+app.delete(`/employees/:id`, (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM Employee WHERE EmpID =?",
+    [req.params.id],
+    (err, rows, field) => {
+      if (!err) res.send("Deleted successfully");
+      else console.log(err);
+    }
+  );
+});
+//Add an new item without input EmpId
+app.post("/employees", (req, res) => {
+  let emp = req.body;
+  console.log(emp);
+  emp.EmpID = 0;
+
+  let sql =
+    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+  CALL EmployeeAddOrEdit(@EmpID, @Name, @EmpCode, @Salary)";
+  mysqlConnection.query(
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
     (err, rows, field) => {
       if (!err) res.send(emp);
-      else console.log(err)
+      else console.log(err);
     }
-  )
-})
+  );
+});
 //Update an item by EmpID.
-app.put('/employees', (req, res) => {
+app.put("/employees", (req, res) => {
   let emp = req.body;
-  
-  let sql = 'SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-  CALL EmployeeAddOrEdit(@EmpID, @Name, @EmpCode, @Salary)';
+
+  let sql =
+    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+  CALL EmployeeAddOrEdit(@EmpID, @Name, @EmpCode, @Salary)";
   mysqlConnection.query(
-    sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
     (err, rows, field) => {
-      if (!err) res.send('Updated successfully');
-      else console.log(err)
+      if (!err) res.send("Updated successfully");
+      else console.log(err);
     }
-  )
-})
-
-
-
+  );
+});
 
 //Server
 const port = 4000;
